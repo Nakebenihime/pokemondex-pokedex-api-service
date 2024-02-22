6c5addc6901fbe7fd3430e1fdbba1c2ffd204908
Nakebenihime <mario-dacosta@hotmail.fr>
2024-02-22 17:01:28 +0100

    refactor: generic reformat code

--- src/main/java/org/example/pokedexapiinterface/viewmodel/PokemonTypeChartsDTO.java
+++ src/main/java/org/example/pokedexapiinterface/viewmodel/PokemonTypeChartsDTO.java
@@ -13,6 +13,10 @@ public class PokemonTypeChartsDTO {
     @NotNull(message = "The \"generation\" field is mandatory.")
     @Min(value = 1, message = "The \"generation\" field must be an integer equal to or greater than 0 and is mandatory.")
     private int generation;
+
+    @NotNull(message = "The \"attack\" object is mandatory.")
     private Map<PokemonType, Double> attack;
+
+    @NotNull(message = "The \"defense\" object is mandatory.")
     private Map<PokemonType, Double> defense;
 }
