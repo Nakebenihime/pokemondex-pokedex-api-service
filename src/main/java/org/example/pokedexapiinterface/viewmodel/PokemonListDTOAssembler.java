a70a7130b29d4073f96406727dca4b4a878066d7
Mario DA COSTA <mario-dacosta@hotmail.fr>
2024-03-03 17:14:09 +0100

    feat(pagination): adding pagination links to PokemonController such as First(), Next(), Previous() or Last()

--- src/main/java/org/example/pokedexapiinterface/viewmodel/PokemonListDTOAssembler.java
+++ src/main/java/org/example/pokedexapiinterface/viewmodel/PokemonListDTOAssembler.java
@@ -5,10 +5,14 @@ import org.example.pokedexapiinterface.controller.PokemonController;
 import org.example.pokedexapiinterface.model.Pokemon;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import java.util.Collection;
 import java.util.stream.StreamSupport;
@@ -47,6 +51,52 @@ public class PokemonListDTOAssembler extends RepresentationModelAssemblerSupport
         PagedModel.PageMetadata pageMetadata = new PagedModel.PageMetadata(page.getSize(), page.getNumber(), page.getTotalElements(), page.getTotalPages());
         PagedModel<PokemonListDTO> pagedModel = PagedModel.of(pokemonListDTOS, pageMetadata);
         pagedModel.add(linkTo(methodOn(PokemonController.class).getPokemons(page.getPageable())).withSelfRel());
+        addQueryParameters(pagedModel, page);
         return pagedModel;
+    }
+
+    private void addQueryParameters(PagedModel<PokemonListDTO> pagedModel, Page<Pokemon> page) {
+        UriComponentsBuilder uriBuilder = buildUriForPagination(page);
+
+        if (!page.isFirst()) {
+            PageRequest firstPageable = PageRequest.of(0, page.getSize(), page.getSort());
+            uriBuilder.replaceQueryParam("page", firstPageable.getPageNumber());
+            uriBuilder.replaceQueryParam("size", firstPageable.getPageSize());
+            addSortingQueryParams(uriBuilder, firstPageable.getSort());
+            pagedModel.add(Link.of(uriBuilder.toUriString(), "first"));
+        }
+
+        if (page.hasNext()) {
+            PageRequest nextPageable = PageRequest.of(page.getNumber() + 1, page.getSize(), page.getSort());
+            uriBuilder.replaceQueryParam("page", nextPageable.getPageNumber());
+            uriBuilder.replaceQueryParam("size", nextPageable.getPageSize());
+            addSortingQueryParams(uriBuilder, nextPageable.getSort());
+            pagedModel.add(Link.of(uriBuilder.toUriString(), "next"));
+        }
+
+        if (page.hasPrevious()) {
+            PageRequest prevPageable = PageRequest.of(page.getNumber() - 1, page.getSize(), page.getSort());
+            uriBuilder.replaceQueryParam("page", prevPageable.getPageNumber());
+            uriBuilder.replaceQueryParam("size", prevPageable.getPageSize());
+            addSortingQueryParams(uriBuilder, prevPageable.getSort());
+            pagedModel.add(Link.of(uriBuilder.toUriString(), "previous"));
+        }
+
+        if (!page.isLast()) {
+            PageRequest lastPageable = PageRequest.of(page.getTotalPages() - 1, page.getSize(), page.getSort());
+            uriBuilder.replaceQueryParam("page", lastPageable.getPageNumber());
+            uriBuilder.replaceQueryParam("size", lastPageable.getPageSize());
+            addSortingQueryParams(uriBuilder, lastPageable.getSort());
+            pagedModel.add(Link.of(uriBuilder.toUriString(), "last"));
+        }
+    }
+
+    private UriComponentsBuilder buildUriForPagination(Page<Pokemon> page) {
+        return linkTo(methodOn(PokemonController.class).getPokemons(page.getPageable())).toUriComponentsBuilder();
+    }
+
+    private void addSortingQueryParams(UriComponentsBuilder uriBuilder, Sort sort) {
+        uriBuilder.replaceQueryParam("sort");
+        sort.forEach(order -> uriBuilder.queryParam("sort", String.format("%s,%s", order.getProperty(), order.getDirection())));
     }
 }
